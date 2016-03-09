d8c4584f2f12d04f645fc47494c1a0a13942e8da
edevignes <etienne.devignes@outlook.fr>
2016-03-09 16:09:02 +0100

    Intégration du DAO et méthode de connexion

--- src/Sio/GsbBundle/Controller/DefaultController.php
+++ src/Sio/GsbBundle/Controller/DefaultController.php
@@ -3,22 +3,40 @@
 namespace Sio\GsbBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use models;
+use Symfony\Component\HTTPFoundation\Request;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
-    {
-        return $this->render('SioGsbBundle:Default:index.html.twig');
-    }
+	public function indexAction()
+	{
 
-    public function connexionAction(Request $request)
-    {
-    	dump($request);
-    	if($request->request->has('valider'))
-    	{
-    		$email = $request->get('email');
-    		$password = $request->get('password');   		
-    	}
-    	return $this->render('SioGsbBundle:Default:index.html.twig');	
-    }
+		return $this->render('SioGsbBundle:Default:index.html.twig', array('msg'=>'trkill'));
+	}
+
+	public function connexionAction(Request $request)
+	{
+		if($request->request->has('valider'))
+		{
+			$msg = "";
+			$mail = $request->get('email');
+			$mdp = $request->get('password');
+			$dao = models\DAOUser::getDaoUser();
+			$res = $dao->getUserById($mail);
+			dump($res[0]['mdp']);
+			if(count($res)==0)
+			{
+				$msg = "identifiant non valide";
+			}else{
+				if ($mdp == $res[0]['mdp'])
+				{
+					$msg = "Connexion réussie";
+				}else{
+					$msg = "Mot de passe incorrect";
+				}
+			}
+
+		}
+		return $this->render('SioGsbBundle:Default:index.html.twig', array('msg'=>$msg));
+	}
 }
