eb05ba3672fd67cd0f846c18ab82eb84a22aca72
lgrisolet <lucas.grisolet@gmail.com>
2016-03-09 17:00:31 +0100

    creations views (layout, dashboard)

--- src/Sio/GsbBundle/Controller/DefaultController.php
+++ src/Sio/GsbBundle/Controller/DefaultController.php
@@ -3,22 +3,42 @@
 namespace Sio\GsbBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use models;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
-    {
-        return $this->render('SioGsbBundle:Default:index.html.twig');
-    }
+	public function indexAction()
+	{
+		return $this->render('SioGsbBundle:Default:index.html.twig', array('user'=>'tto'));
+	}
+	public function dashboardAction()
+	{
+		return $this->render('SioGsbBundle:Default:dashboard.html.twig');
+	}
 
-    public function connexionAction(Request $request)
-    {
-    	dump($request);
-    	if($request->request->has('valider'))
-    	{
-    		$email = $request->get('email');
-    		$password = $request->get('password');   		
-    	}
-    	return $this->render('SioGsbBundle:Default:index.html.twig');	
-    }
+	public function connexionAction(Request $request)
+	{
+		dump($request);
+		if($request->request->has('valider'))
+		{
+			$msg = "";
+			$mail = $request->get('email');
+			$mdp = $request->get('password');
+			$dao = models\DAOUser::getDaoUser();
+			$res = $dao->getUserById($mail);
+			if(count($res == 0))
+			{
+				$msg = "identifiant non valide";
+			}else{
+				if ($mdp == $res['mdp'])
+				{
+					$msg = "Connexion réussie";
+				}else{
+					$msg = "Mot de passe incorrect";
+				}
+			}
+
+		}
+		return $this->render('SioGsbBundle:Default:index.html.twig');
+	}
 }
